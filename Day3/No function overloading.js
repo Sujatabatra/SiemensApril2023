cec910f13f3f59e41c269461f9a2aa8cfaa3e89d
Sujatabatra <sujatabatra@hotmail.com>
2023-04-19 13:18:13 +0530

    Add files via upload

---
+++ Day3/No function overloading.js
@@ -0,0 +1,49 @@
+function print(arg1,arg2,arg3){
+  console.log("print method with three(3) arguments")
+  console.log("arg1 : "+arg1);
+  console.log("arg2 : "+arg2);
+  console.log("arg3 : "+arg3);
+}
+
+function print(arg1,arg2){
+  console.log("print method with two(2) arguments")
+  console.log("arg1 : "+arg1);
+  console.log("arg2 : "+arg2);
+  
+}
+
+function print(arg1,arg2,arg3,arg4,arg5){
+  console.log("print method with five(5) arguments")
+  console.log("arg1 : "+arg1);
+  console.log("arg2 : "+arg2);
+  console.log("arg3 : "+arg3);
+  console.log("arg4 : "+arg4);
+  console.log("arg5 : "+arg5);
+}
+function print(){
+  console.log("print method with zero(0) arguments")
+}
+//print is a variable and type is function
+print();
+print(10,20,30); 
+print(1,2,3,4,5);
+print(1,2);
+print(1,2,3,4,5,6,7,8,9);
+
+
+
+// var x;
+// x=10;
+// x=20;
+// x="Sujata"
+// x="Prabakran"
+// x="Mahesh"
+// console.log(x);
+
+
+
+
+
+
+
+
